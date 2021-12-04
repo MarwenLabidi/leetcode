749f889b2d9765444e84638699a1b71138df98bd
Marwen Labidi <35144939+marwenez@users.noreply.github.com>
2021-12-04 11:47:00 +0100

    Time: 80 ms (45.25%), Space: 40.3 MB (13.20%) - LeetHub

---
+++ invert-binary-tree/invert-binary-tree.js
@@ -0,0 +1,17 @@
+
+var invertTree = function(root) {
+    invert(root);
+    return root;
+};
+
+function invert(root){
+    if(!root)
+        return
+    
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    
+    invert(root.left);
+    invert(root.right);
+}
